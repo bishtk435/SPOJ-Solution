a60102ef6c8128a02d75167118dd913474548087
Kamal Bisht <39944015+bisthk435@users.noreply.github.com>
2018-06-28 19:38:46 +0530

    Create TEST - Life, the Universe, and Everything.cpp

---
+++ TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short a;
+	for(int i=0;i<5;i+=0)
+	{
+		cin>>a;
+		if(a==42)
+            i+=5;
+        else
+            cout<<a<<endl;
+    }
+ 
+	return 0;
+}
